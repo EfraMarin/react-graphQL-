f6321d46cbbd23f8ed004a9d1b4244f6693d1f1c
Efra Marin <efrain.marin@encora.com>
2024-01-20 16:16:32 -0600

    graphQL packages and first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from "@apollo/client";
 
 function App() {
+
+  const GET_LOCATIONS = gql`
+  query {
+    characters{
+      results{
+        id name type species image
+      }
+    }
+  }
+`;
+
+  function DisplayLocations() {
+    const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+    console.log(data.characters.results);
+
+    return data.characters.results.map(({ name, species, id, image }) => (
+      <div key={id}>
+        <h3>{name}</h3>
+        <img width="400" height="250" alt="location-reference" src={`${image}`} />
+        <br />
+        <b>About this location:</b>
+        <p>{species}</p>
+        <br />
+      </div>
+    ));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <br />
+      <DisplayLocations />
     </div>
   );
 }
